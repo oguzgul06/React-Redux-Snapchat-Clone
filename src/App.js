4f049c18caa67326c163d9a9a13cbcf1090e7607
oguz gül <ogzgl95@outlook.com>
2021-08-28 14:06:14 +0300

    WebcamCapture components adding to project for webcam configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import WebcamCapture from "./components/WebcamCapture";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build Snapchat🚀</h1>
+      <WebcamCapture />
     </div>
   );
 }
