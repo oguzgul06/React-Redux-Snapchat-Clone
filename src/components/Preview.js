a8305e74d9b61fe2f1934a01e023380e096b8d7c
oguz gül <ogzgl95@outlook.com>
2021-08-28 15:52:13 +0300

    Design style on the preview copmponent

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,6 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { resetCameraImage, selectCameraImage } from "../features/cameraSlice";
 import CloseIcon from "@material-ui/icons/Close";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
+import SendIcon from "@material-ui/icons/Send";
 import "./Preview.css";
 
 function Preview() {
@@ -18,13 +26,28 @@ function Preview() {
 
   const closePreview = () => {
     dispatch(resetCameraImage());
-    
   };
 
   return (
     <div className="preview">
       <CloseIcon onClick={closePreview} className="preview__close" />
+
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
+
       <img src={cameraImage} alt="" />
+
+      <div className="preview__footer">
+        <h2>Send Now</h2>
+        <SendIcon fontSize="small" className="preview__sendIcon" />
+      </div>
     </div>
   );
 }
