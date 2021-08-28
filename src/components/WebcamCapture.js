4bcfebcbb7d62cd2691c9a13b63f2474c1617a5e
oguz gül <ogzgl95@outlook.com>
2021-08-28 14:35:22 +0300

    Create cameraSlice for redux with dispatch on the WebcamCapture

--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -1,6 +1,9 @@
-import React, { useRef } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import "./WebcamCapture.css";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+import { useDispatch } from "react-redux";
+import { setCameraImage } from "../features/cameraSlice";
 
 const videoConstraints = {
   width: 250,
@@ -10,6 +13,12 @@ const videoConstraints = {
 
 function WebcamCapture() {
   const webcamRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    dispatch(setCameraImage(imageSrc));
+  }, [webcamRef]);
 
   return (
     <div className="webcamCapture">
@@ -20,6 +29,12 @@ function WebcamCapture() {
         screenshotFormat="image/jpeg"
         width={videoConstraints.width}
         videoConstraints={videoConstraints}
+      />
+
+      <RadioButtonUncheckedIcon
+        className="webcamCapture__button"
+        onClick={capture}
+        fontSize="large"
       />
     </div>
   );
