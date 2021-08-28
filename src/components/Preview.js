f50d40c745343cbc12635d115e8a0c231f2f1cea
oguz gül <ogzgl95@outlook.com>
2021-08-28 15:21:34 +0300

    Reset and Select camera image configuration

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import "./Preview.css"
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { resetCameraImage, selectCameraImage } from "../features/cameraSlice";
+import CloseIcon from "@material-ui/icons/Close";
+import "./Preview.css";
 
 function Preview() {
-    return (
-        <div className="preview">
-            
-        </div>
-    )
+  const cameraImage = useSelector(selectCameraImage);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!cameraImage) {
+      history.replace("/");
+    }
+  }, [cameraImage, history]);
+
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+    
+  };
+
+  return (
+    <div className="preview">
+      <CloseIcon onClick={closePreview} className="preview__close" />
+      <img src={cameraImage} alt="" />
+    </div>
+  );
 }
 
-export default Preview
+export default Preview;
