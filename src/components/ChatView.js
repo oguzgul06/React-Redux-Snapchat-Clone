d9c2f73de8b005c2240ade6a642c623528a10d66
oguz gül <ogzgl95@outlook.com>
2021-08-30 13:02:55 +0300

    Return to chatview from chats

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { selectSelectedImage } from "../features/appSlice";
 import "./ChatView.css";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 function ChatView() {
   const selectedImage = useSelector(selectSelectedImage);
@@ -21,6 +22,24 @@ function ChatView() {
   return (
     <div className="chatView">
       <img onClick={exit} src={selectedImage} alt="" />
+      <CountdownCircleTimer
+        isPlaying
+        duration={10}
+        strokeWidth={6}
+        size={50}
+        colors={[
+          ["#004777", 0.33],
+          ["#F78801", 0.33],
+          ["#A30000", 0.33],
+        ]}
+      >
+        {({ remainingTime }) => {
+          if (remainingTime === 0) {
+            exit();
+          }
+          return remainingTime;
+        }}
+      </CountdownCircleTimer>
     </div>
   );
 }
